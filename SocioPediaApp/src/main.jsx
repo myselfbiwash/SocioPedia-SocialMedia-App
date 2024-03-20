90fed54cf7634bdcccdf3bdde4690bdeb5b77bdc
Biwash Budhathoki <075bct025@ioepc.edu.np>
2024-03-20 17:44:17 +0545

    Add font and background color to body element, update dependencies in package.json, update title in index.html, update server configuration in vite.config.js, and add comments in main.jsx

--- SocioPediaApp/src/main.jsx
+++ SocioPediaApp/src/main.jsx
@@ -8,3 +8,13 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+
+// ReactDOM.createRoot(document.getElementById('root')): This line is using the createRoot method from the ReactDOM library to create a root React 
+// component attached to the HTML element with the id 'root'. This is typically a <div> element in your HTML file.
+//  .render(): This method is used to render a React element into the root DOM node. In other words, it's where your React app starts and where it 
+//  will be inserted in the DOM.
+//  <React.StrictMode>: This is a wrapper component that checks for potential problems in the app during development. It does not render 
+//   any visible UI. It activates additional checks and warnings for its descendants.
+//  <App />: This is the main component of your React application. All other components will likely be descendants of this component.
+//  So, in summary, this code is creating a root React component, and rendering your <App /> component inside of it, with additional checks and 
+//  warnings activated by <React.StrictMode>
