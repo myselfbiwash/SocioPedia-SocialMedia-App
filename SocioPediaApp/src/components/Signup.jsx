df81140c9c775d590531232df8bbbf4b6f16d36d
Biwash Budhathoki <075bct025@ioepc.edu.np>
2024-03-21 22:01:41 +0545

    Update changed css name

--- SocioPediaApp/src/components/Signup.jsx
+++ SocioPediaApp/src/components/Signup.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Signup.css";
+import "./Entry.css";
 
 const SignUp = () => {
   const [firstName, setFirstName] = useState("");
