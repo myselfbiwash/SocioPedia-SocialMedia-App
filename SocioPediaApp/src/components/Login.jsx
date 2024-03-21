0e6e91f9958d8287c7ab6e09ce6281347623f5e7
Biwash Budhathoki <075bct025@ioepc.edu.np>
2024-03-21 22:00:52 +0545

    Create PrivateComponent to hold place if authenticated else proceed to login

--- SocioPediaApp/src/components/Login.jsx
+++ SocioPediaApp/src/components/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import "./Signup.css";
 
 const Login =()=>{
     const [email,setEmail] = React.useState('');
@@ -31,11 +32,14 @@ const Login =()=>{
 
     }
     return(
-        <div className="login">
+        <div className="register-login">
+        <h1>Sociopedia</h1>
+        <div className="signup-login">
             <input type="text" className="inputBox" placeholder="Enter Email" onChange={(e)=>setEmail(e.target.value)} value ={email} />
             <input type="password" className="inputBox" placeholder="Enter Password" onChange={(e)=>setPassword(e.target.value)} value ={password} />
             <button onClick={handleLogin} className="appButton" type="button">Login</button>
 
+        </div>
         </div>
     )
 
