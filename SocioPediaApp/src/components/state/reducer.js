871d34c41e348ea0dd00a0fbdcf2ebb3e470871f
Biwash Budhathoki <075bct025@ioepc.edu.np>
2024-03-22 23:20:28 +0545

    Add authSlice reducer to handle login and logout actions

---
+++ SocioPediaApp/src/components/state/reducer.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: { user: null, token: null },
+  reducers: {
+    login: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+    logout: (state) => {
+        state.user = null;
+        state.token = null;
+      },
+    // add other reducers like logout here
+  },
+});
+
+export const { login,logout } = authSlice.actions;
+
+export default authSlice.reducer;
