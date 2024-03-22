f63694e3713c379d4b766a1cce7b9d51793d8099
Biwash Budhathoki <075bct025@ioepc.edu.np>
2024-03-22 23:22:16 +0545

    Refactor auth to access auth from redux store instead of local storage

--- SocioPediaApp/src/components/PrivateComponent.jsx
+++ SocioPediaApp/src/components/PrivateComponent.jsx
@@ -1,8 +1,12 @@
 import React  from "react";
 import {Navigate, Outlet} from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux';
+
 
 const PrivateComponent=()=>{
-    const auth = localStorage.getItem('user');
+    const auth = useSelector(state => state.auth.user);
+
+    // const auth = localStorage.getItem('user');
     return auth?<Outlet />:<Navigate to="/login" />
 }
 
