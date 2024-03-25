b4b4763f213999e5db40ae756369e05dd21d8776
Biwash Budhathoki <075bct025@ioepc.edu.np>
2024-03-25 18:31:34 +0545

    Refactor PostWidget component

--- SocioPediaApp/src/components/widgets/PostWidget.jsx
+++ SocioPediaApp/src/components/widgets/PostWidget.jsx
@@ -1,55 +1,57 @@
-import React from 'react'
+import React from "react";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setPost } from "../state/reducer";
 import Friend from "../../components/Friend";
-
+import './Widget.css'
 
 const PostWidget = ({
-    postId,
-    postUserId,
-    name,
-    description,
-    location,
-    picturePath,
-    userPicturePath,
-    likes,
-    comments,
-  }) => {
-    const [isComments, setIsComments] = useState(false);
-    const dispatch = useDispatch();
-    const token = useSelector((state) => state.auth.token);
-    const loggedInUserId = useSelector((state) => state.auth.user._id);
-    const isLiked = Boolean(likes[loggedInUserId]);
-    const likeCount = Object.keys(likes).length;
+  postId,
+  postUserId,
+  name,
+  description,
+  location,
+  picturePath,
+  userPicturePath,
+  likes,
+  comments,
+}) => {
+  const [isComments, setIsComments] = useState(false);
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.auth.token);
+  const loggedInUserId = useSelector((state) => state.auth.user._id);
+  const isLiked = Boolean(likes[loggedInUserId]);
+  const likeCount = Object.keys(likes).length;
 
-    const patchLike = async () => {
-        const response = await fetch(`http://localhost:3001/posts/${postId}/like`, {
-          method: "PATCH",
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ userId: loggedInUserId }),
-        });
-        const updatedPost = await response.json();
-        dispatch(setPost({ post: updatedPost }));
-      };
+  const patchLike = async () => {
+    const response = await fetch(`http://localhost:3001/posts/${postId}/like`, {
+      method: "PATCH",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userId: loggedInUserId }),
+    });
+    const updatedPost = await response.json();
+    dispatch(setPost({ post: updatedPost }));
+  };
 
   return (
-    <div>
+    <div className="post-widget">
       <Friend
         friendId={postUserId}
         name={name}
         subtitle={location}
         userPicturePath={userPicturePath}
       />
-        <div>
-            <p>{description}</p>
-            {picturePath && <img src={picturePath} alt="post" />}
-            </div>
+      <div className="post-widget-middle">
+        <p>{description}</p>
+        {picturePath && (
+          <img style={{width:'600px', height:'700px'}} src={`http://localhost:4000/assets/${picturePath}`} alt="post" />
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PostWidget
+export default PostWidget;
