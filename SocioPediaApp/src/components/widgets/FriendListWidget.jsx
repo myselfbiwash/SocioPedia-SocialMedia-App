cf169afd80693756c214d4e462b700b649556e8d
Biwash Budhathoki <075bct025@ioepc.edu.np>
2024-03-28 13:30:43 +0545

    Refactor FriendListWidget component to fetch and display user's friends

--- SocioPediaApp/src/components/widgets/FriendListWidget.jsx
+++ SocioPediaApp/src/components/widgets/FriendListWidget.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import Friend from "../../components/Friend";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setFriends } from "../state/reducer";
 
-const FriendListWidget = () => {
-  return (
-    <div>
-      
+const FriendListWidget = ({ userId }) => {
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.auth.token);
+  const friends = useSelector((state) => state.auth.user.friends);
+
+  const getFriends = async () => {
+    const response = await fetch(
+      `http://localhost:3001/users/${userId}/friends`,
+      {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+    const data = await response.json();
+    dispatch(setFriends({ friends: data }));
+  };
+
+  useEffect(() => {
+    getFriends();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return(
+    <div className="friend-list">
+       <div>Friend List: </div>
+         <div>
+        {friends.map((friend) => (
+          <Friend
+            key={friend._id}
+            friendId={friend._id}
+            name={`${friend.firstName} ${friend.lastName}`}
+            subtitle={friend.occupation}
+            userPicturePath={friend.picturePath}
+            isFriendList={true}
+          />
+        ))}
+        </div>
     </div>
-  )
-}
-
-export default FriendListWidget
+  );
+};
+export default FriendListWidget;
